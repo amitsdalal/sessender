8292d3929858d553e883cddddd5828e516f4e670
Amit Dalal <ad@webscoot.io>
2023-03-28 14:34:12 +0530

    feat: Added Readme for pypi.
    Add logging and error handling to Sessender class
    
    This commit adds robust logging and error handling to the Sessender class, making it easier for developers to troubleshoot issues and diagnose problems when sending emails with SES. The changes made in this commit include:
    
    - Wrapping SMTP credential retrieval in a try-except block to handle errors
    - Adding a logging file to the Sessender class that logs SMTP activity
    - Updating the README to include information about how to use the logging feature
    - Fixing a bug where the recipient and sender addresses were not being properly assigned
    
    These changes should improve the overall usability and stability of the Sessender package, making it a more reliable and efficient way to send emails with SES.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='sessender',
-    version='0.2',
-    author='Amit Dalal',
-    description='A package for sending emails using Amazon SES',
-    py_modules=['sessender'],
+    name="sessender",
+    version="0.3",
+    author="Amit Dalal",
+    author_email="amit+pypi@serverlessguy.com",
+    description="A Python package for sending emails using AWS SES",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/sessender",
+    packages=['sessender'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
