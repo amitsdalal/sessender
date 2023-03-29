697d9add82eb7245543e7cc72e804ac558dd2583
Amit Dalal <ad@webscoot.io>
2023-03-29 16:33:36 +0530

    Actions for relase on tag release

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
-
+import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+
+# Extract tag name from GITHUB_REF environment variable
+tag = os.environ['GITHUB_REF'].split('/')[-1]
+if tag.startswith('v'):
+    version = tag[1:]
+else:
+    version = tag
+
 setup(
     name="sessender",
-    version="0.4",
+    version=version,
     author="Amit Dalal",
     description="A Python package for sending emails using AWS SES",
     long_description=long_description,
